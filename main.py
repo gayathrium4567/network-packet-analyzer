a7bef25742036d70c693798b37c0113d60d018ca
gayathrium4567 <umgayathri207@gmail.com>
2026-06-06 13:35:08 +0530

    Day 2: Packet details extraction implemented

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from scapy.all import sniff
+from scapy.all import sniff, IP, TCP, UDP, ICMP
 
 def packet_callback(packet):
-    print(packet.summary())
+    if IP in packet:
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
+        protocol = packet[IP].proto
+
+        if TCP in packet:
+            src_port = packet[TCP].sport
+            dst_port = packet[TCP].dport
+            print(f"TCP | {src_ip}:{src_port} -> {dst_ip}:{dst_port}")
+
+        elif UDP in packet:
+            src_port = packet[UDP].sport
+            dst_port = packet[UDP].dport
+            print(f"UDP | {src_ip}:{src_port} -> {dst_ip}:{dst_port}")
+
+        elif ICMP in packet:
+            print(f"ICMP | {src_ip} -> {dst_ip}")
 
 print("Starting packet capture...")
-sniff(prn=packet_callback, count=10)
+sniff(prn=packet_callback, count=20)
